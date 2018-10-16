d0310016046f196d09c1674d9745365569a1fe85
杨生 <289536143@qq.com>
2018-10-16 16:01:33 +0800

    初始化项目模块

---
+++ src/main/java/com/yang/blog/website/dao/RelationshipVoMapper.java
@@ -0,0 +1,27 @@
+package com.yang.blog.website.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import com.yang.blog.website.model.Vo.RelationshipVoExample;
+import com.yang.blog.website.model.Vo.RelationshipVoKey;
+
+@Component
+public interface RelationshipVoMapper {
+    long countByExample(RelationshipVoExample example);
+
+    int deleteByExample(RelationshipVoExample example);
+
+    int deleteByPrimaryKey(RelationshipVoKey key);
+
+    int insert(RelationshipVoKey record);
+
+    int insertSelective(RelationshipVoKey record);
+
+    List<RelationshipVoKey> selectByExample(RelationshipVoExample example);
+
+    int updateByExampleSelective(@Param("record") RelationshipVoKey record, @Param("example") RelationshipVoExample example);
+
+    int updateByExample(@Param("record") RelationshipVoKey record, @Param("example") RelationshipVoExample example);
+}
